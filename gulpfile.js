e416c8b8e355f2b35d0797f335d9f489151a35b5
ev_massive <ev@massive.ag>
2018-12-10 16:06:56 -0300

    Inicio del proyecto

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const pug = require('gulp-pug');
+
+gulp.task('sass', () => {
+  return gulp.src([
+    'node_modules/bootstrap/scss/bootstrap.scss',
+    'src/scss/*.scss'
+  ])
+  .pipe(sass({outputStyle: 'compressed'}))
+  .pipe(gulp.dest('dist/css'))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('js', () => {
+  return gulp.src([
+    'node_modules/bootstrap/dist/js/bootstrap.min.js',
+    'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/popper.js/dist/umd/popper.min.js'
+  ])
+  .pipe(gulp.dest('dist/js'))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('serve', ['sass'], () => {
+  browserSync.init({
+    server: './src'
+  });
+
+  gulp.watch([
+    'node_modules/bootstrap/scss/bootstrap.min.scss',
+    'src/scss/*.scss'
+  ], ['sass']);
+
+  gulp.watch('dist/*.html').on('change', browserSync.reload);
+
+});
+
+gulp.task('font-awesome', () => {
+  return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+  .pipe(gulp.dest('dist/css'));
+})
+
+gulp.task('fonts', () => {
+  return gulp.src('node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('default', ['js', 'serve', 'font-awesome', 'fonts'])
